d22012013b3fccc6172565809ee5004688adb901
Yuen Ler Chow <lerchow@gmail.com>
2023-12-21 01:25:19 -0500

    update openai to new version

--- ai_predictions.py
+++ ai_predictions.py
@@ -1,5 +1,4 @@
-import openai
-import os
+from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,34 +9,47 @@ def get_ai_predictions(news_headlines):
     Input: List of tuples (headline, datePublished).
     Returns: JSON string containing buy/sell recommendations for each stock and confidence metric.
     """
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    client = OpenAI()
+
+    seed = 82
 
     # First prompt to get general stock trends
-    combined_headlines = '. '.join([headline for headline, _ in news_headlines])
-    initial_prompt = f"I'm going to give you news headlines and you will tell me what stocks to buy. " \
-                     f"Consider the industries these companies depend on and the companies that invest in " \
-                     f"them. Give me a list of stocks and a short prediction for each of them.\n\n" \
-                     f"NEWS DATA:\n{combined_headlines}"
+    combined_headlines = ' \n'.join([headline for headline, _ in news_headlines])
+    system_prompt = f"The user will give you news headlines and you will tell them what stocks to buy. " \
+                     f"Go beyond the companies mentioned in the headlines and consider factors like what" \
+                     f"industries these companies depend on and the companies that invest in " \
+                     f"them. Give them a list of stocks and a short prediction for each of them.\n\n" \
 
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=initial_prompt,
-      max_tokens=250
+    response = client.chat.completions.create(
+      model="gpt-4",
+      messages=[
+          {'role': 'system', 'content': system_prompt},
+          {'role': 'user', 'content': combined_headlines},
+      ],
+      max_tokens=500,
+      seed=seed,
     )
 
-    general_trends = response.choices[0].text.strip()
+    general_trends = response['choices'][0]['message']['content']
 
     # Second prompt to get buy/sell recommendations in a specific JSON format
-    follow_up_prompt = f"Based on the previous analysis, provide buy/sell recommendations for each stock " \
-                       f"mentioned in a JSON format. The JSON should have the format: " \
+    follow_up_prompt = f"Based on your analysis, can you provide buy/sell recommendations for each stock " \
+                       f"you mentioned in a JSON format? The JSON should have the format: " \
                        f"{{'stock_symbol': {{'action': 'buy/sell', 'confidence': percentage}}}}."
 
-    response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=follow_up_prompt,
-      max_tokens=250
+    response = client.chat.completions.create(
+      model="gpt-4",
+      response_format={ "type": "json_object" },
+      messages=[
+          {'role': 'system', 'content': system_prompt},
+          {'role': 'user', 'content': combined_headlines},
+          {'role': 'assistant', 'content': general_trends},
+          {'role': 'user', 'content': follow_up_prompt},
+      ],
+      max_tokens=200,
+      seed=seed,
     )
 
-    recommendations_json = response.choices[0].text.strip()
+    recommendations_json = response['choices'][0]['message']['content']
 
     return general_trends, recommendations_json
