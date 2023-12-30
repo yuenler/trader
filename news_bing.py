483d17c8ff9aa02bc4492017e6425a9f460bc4ee
Edward Kang <44958279+ekang7@users.noreply.github.com>
2023-12-30 08:54:50 -0600

    added bing search

--- news_bing.py
+++ news_bing.py
@@ -4,15 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_news_from_bing(is_first_run_of_day, num_minutes_between_runs):
+subscription_key = os.getenv('BING_API_KEY')
+search_url = "https://api.bing.microsoft.com/v7.0/news/search"
+headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+
+def get_news_from_bing(search_term):
     """
     Fetches the latest news from Bing News Search API.
     Returns: List of tuples (headline, datePublished).
     """
-    url = "https://api.bing.microsoft.com/v7.0/news/search"
-    headers = {'Ocp-Apim-Subscription-Key': os.getenv('BING_API_KEY')}
-    params = {'q': 'tech sector', 'count': 10, 'sortBy': 'Date'}
-    response = requests.get(url, headers=headers, params=params)
+    params = {'q': search_term, 'count': 10, 'sortBy': 'Date'}
+    response = requests.get(search_url, headers=headers, params=params)
     news_data = response.json()
     news_headlines = [(item['name'], item['datePublished']) for item in news_data['value']]
+    # for x in news_headlines:
+    #     print(x)
     return news_headlines
+
+sample = get_news_from_bing('Microsoft')
+print(sample)
